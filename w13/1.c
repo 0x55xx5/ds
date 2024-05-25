72078052398a32e6eafdfdc4794d793533c8c12e
Kevin Yu <jsyu17@gmail.com>
2024-05-25 22:46:00 +0800

    w13

---
+++ w13/1.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int arr[6][6] = {0}, i, j, k, tmpi, tmpj;
+    int data[10][2] = {{1, 2}, {2, 1}, {1, 5}, {5, 1}, {2, 3}, {3, 2}, {2, 4}, {4, 2}, {3, 4}, {4, 3}};
+    for (i = 0; i < 10; i++)
+        for (j = 0; j < 6; j++)
+            for (k = 0; k < 6; k++)
+            {
+                tmpi=data[i][0];
+                tmpj=data[i][1];
+                arr[tmpi][tmpj] = 1;
+            }
+    printf("無向圖: \n");
+    for (i = 1; i < 6; i++)
+    {
+         for (j = 1; j < 6; j++)
+                printf("[%d]", arr[i][j]); printf("\n");
+    }
+   
+   
+    system("pause");
+    return 0;
+}
+/*
+
+無向圖: 
+[0][1][0][0][1]
+[1][0][1][1][0]
+[0][1][0][1][0]
+[0][1][1][0][0]
+[1][0][0][0][0]
+*/
---
+++ w13/3.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+struct list
+{
+    int val;
+    struct list *next;
+};
+typedef struct list node;
+typedef node *link;
+struct list head[6];
+/*宣告一個節點型態陣列*/
+int main()
+{
+    link ptr, newnode;
+    char data[14][2] = {{1, 2}, {2, 1}, {2, 5}, {5, 2}, {2, 3}, {3, 2}, {2, 4}, {4, 2}, {3, 4}, {4, 3}, {3, 5}, {5, 3}, {4, 5}, {5, 4}};
+    int i, j;
+    printf("content: \n");
+    printf("--\n");
+    for (i = 1; i < 6; i++)
+    {
+        head[i].val = i;
+        head[i].next = NULL;
+        printf("頂點 %d =>", i);
+        ptr = &(head[i]);
+        for (j = 0; j < 14; j++)
+        {
+            if (data[j][0] == 1)
+            {
+                newnode = (link)malloc(sizeof(node));
+                newnode->val = data[j][1];
+                newnode->next = NULL;
+                while (ptr != NULL)
+                    ptr = ptr->next;
+                ptr = newnode;
+                printf("[%d]", newnode->val);
+            }
+        }
+        printf("\n");
+    }
+    system("pause");
+    return 0;
+}
+/*
+
+content: 
+--
+頂點 1 =>[2]
+頂點 2 =>[2]
+頂點 3 =>[2]
+頂點 4 =>[2]
+頂點 5 =>[2]
+sh: pause: command not found
+*/
